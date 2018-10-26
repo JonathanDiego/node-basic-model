3379a78115a8f3424630b1ef75d7110fbfee2967
JonathanDiego <resposta42@gmail.com>
2018-10-26 09:37:23 -0300

    Delete commit.bat
    no need in repository

--- commit.bat
+++
@@ -1,9 +0,0 @@
-echo "# node-basic-model" >> README.md
-git config --global user.email "resposta42@gmail.com"
-git config --global user.name "JonathanDiego"
-git init
-git add *.*
-git commit -m "first commit"
-git remote add origin https://github.com/JonathanDiego/node-basic-model.git
-git push -u origin master
-pause
